e7d4bd7ad1a24f1553c6bb6ea0a1000e487b5715
Tania Tasmin <taniat.kaau@email.com>
2021-12-07 17:28:33 -0500

    Refactor to pass the tests

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -11,7 +11,9 @@ export default function DayListItem(props) {
   return (
     <li className={dayClass} onClick={() => {props.setDay(props.name)}}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      {props.spots>1 && <h3 className="text--light">{props.spots} spots remaining</h3>}
+      {props.spots===1 && <h3 className="text--light">1 spot remaining</h3>}
+      {props.spots===0 && <h3 className="text--light">no spots remaining</h3>}
     </li>
   );
 }
