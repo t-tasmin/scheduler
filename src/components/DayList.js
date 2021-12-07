a89a20e14d03c2a961f7cb14aabb591c02ea1ecd
Tania Tasmin <taniat.kaau@email.com>
2021-12-07 18:14:08 -0500

    Add Daylist component

---
+++ src/components/DayList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
+
+export default function DayList(props) {
+  
+  return (
+    <ul>
+      
+    </ul>
+  );
+}
