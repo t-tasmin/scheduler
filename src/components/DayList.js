7d7b82e7ba505f4d27d9ffa3a198d23c6da06b9b
Tania Tasmin <taniat.kaau@email.com>
2021-12-07 19:43:04 -0500

    Create DayListItemArray array

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,13 +1,23 @@
 import React from "react";
 import "components/DayListItem.scss";
-import classNames from "classnames";
+import DayListItem from "components/DayListItem";
 
 
 export default function DayList(props) {
-  
+
+ const DayListItemArray = props.days.map((element)=>{
+                         return (<DayListItem 
+                          key={element.id}
+                          name={element.name} 
+                          spots={element.spots} 
+                          selected={element.name === props.day}
+                          setDay={props.setDay}  
+                         />)
+  });
+
   return (
     <ul>
-      
-    </ul>
+      {DayListItemArray}
+   </ul>
   );
 }
