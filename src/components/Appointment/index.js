7de6f4425927ebe128400f8796253c6dcdbb7b76
Tania Tasmin <taniat.kaau@email.com>
2021-12-09 08:30:49 -0500

    Add Show and Empty Components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
 
 
 export default function Appointment(props) {
    
    return (
     <article className="appointment">
-       {props.time ? <span> Appointment at {props.time} </span> : <span>No Appointments</span>}
+      <header> {props.time} </header>
+       {props.interview ?  <Show student={props.interview.student} interviewer={props.interview.interviewer}/> :<Empty/>} 
      </article>
    );
  }
