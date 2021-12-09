5d4fa5dcbf58b2e2010ef78a2dcc442569118a4f
Tania Tasmin <taniat.kaau@email.com>
2021-12-09 10:31:46 -0500

    Add appointments array and map into Application Component

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,29 +2,72 @@ import React from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import  { useState } from "react";
+import  Appointment from "components/Appointment";
+
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
 
 export default function Application(props) {
 
   const [day, setDay] = useState("Monday");
 
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
-
+  const appointment = appointments.map(appointment => <Appointment  {...appointment} key={appointment.id} />);
+  
   return (
     <main className="layout">
       <section className="sidebar">
@@ -48,7 +91,8 @@ export default function Application(props) {
          />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+         {appointment}
+         <Appointment key="last" time="5pm" />
       </section>
       
         
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,7 +4,6 @@ import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
 
-
 export default function Appointment(props) {
    
    return (
