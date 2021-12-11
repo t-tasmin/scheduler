17efc36ab5f830c516128c596a75ad76a6751d3b
Tania Tasmin <taniat.kaau@email.com>
2021-12-11 05:28:06 -0500

    Change the 2nd function to fix a bug

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,46 +1,3 @@
-const state = {
-  days: [
-    {
-      id: 1,
-      name: "Monday",
-      appointments: [1, 2, 3]
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      appointments: [4, 5]
-    }
-  ],
-
-  appointments: {
-    "1": { id: 1, time: "12pm", interview: null },
-    "2": { id: 2, time: "1pm", interview: null },
-    "3": {
-      id: 3,
-      time: "2pm",
-      interview: { student: "Archie Cohen", interviewer: 2 }
-    },
-    "4": { id: 4, time: "3pm", interview: null },
-    "5": {
-      id: 5,
-      time: "4pm",
-      interview: { student: "Chad Takahashi", interviewer: 2 }
-    }
-  },
-
-  interviewers: {
-    "1": {  
-      "id": 1,
-      "name": "Sylvia Palmer",
-      "avatar": "https://i.imgur.com/LpaY82x.png"
-    },
-    "2": {
-      id: 2,
-      name: "Tori Malcolm",
-      avatar: "https://i.imgur.com/Nmx0Qxo.png"
-    }
-  }
-};
 
 export function getAppointmentsForDay(state, day) {
   let appointments_id = [];
@@ -65,12 +22,14 @@ export function getAppointmentsForDay(state, day) {
   return appointments;
 };
 
-export function getInterview(state, interview){
-  if (interview === null){
+export function getInterview(state, interview) {
+  if (!interview) {
     return null;
   }
-  interview.interviewer=state.interviewers[interview.interviewer];
-  return interview;
-}
 
-console.log(getInterview(state, state.appointments["3"].interview));
+  const interviewerInfo = state.interviewers[interview.interviewer];
+  return {
+    student: interview.student,
+    interviewer: interviewerInfo
+  }
+};
